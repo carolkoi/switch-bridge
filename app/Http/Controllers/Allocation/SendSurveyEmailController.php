d98c4207443e7e93c87ab24a6ceb8a7f990baaf1
carolkoi <gitongacarol11@gmail>
2019-11-22 18:50:00 +0300

    queuing emails

--- app/Http/Controllers/Allocation/SendSurveyEmailController.php
+++ app/Http/Controllers/Allocation/SendSurveyEmailController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Allocation;
 
 use App\Http\Controllers\Controller;
 use App\Mail\SurveyEmail;
+use App\Jobs\SendSurveyEmailJob;
 use App\Models\Allocation;
 use App\Models\Client;
 use App\Models\Options;
@@ -19,6 +20,21 @@ use Webpatser\Uuid\Uuid;
 class SendSurveyEmailController extends Controller
 {
     //
+//    public function setEmailDelay(){
+//        //Delaying email jobs
+//        $baseDelay = json_encode(now());
+//        $getDelay = json_encode(
+//            cache('jobs.' . SendSurveyEmailJob::class, $baseDelay)
+//        );
+//        $setDelay = Carbon::parse(
+//            $getDelay->date
+//        )->addSeconds(10);
+//
+//        cache([
+//            'jobs.' . SendSurveyEmailJob::class => json_encode($setDelay)
+//        ], 5);
+//        return;
+//    }
     public function emailSurvey($id)
     {
         $setting = Options::where('option_name', 'automatic_survey_send')->first();
@@ -55,7 +71,9 @@ class SendSurveyEmailController extends Controller
             $template = Template::find($staff->template_id);
             $staff_email = User::find($staff->user_id)->email;
             $token = Uuid::generate()->string;
-            Mail::to($staff_email)->send(new SurveyEmail($template, $token));
+//            Mail::to($staff_email)->send(new SurveyEmail($template, $token));
+//            SendSurveyEmailJob::dispatch($template, new SurveyEmail($template, $token))->delay(1);
+            $this->dispatch(new SendSurveyEmailJob($template,$token,$staff_email));
             Allocation::where('user_id', $staff->user_id)->update(['email_sent' => 1]);
 
         }
@@ -72,8 +90,13 @@ class SendSurveyEmailController extends Controller
             $template = Template::find($client->template_id);
             $client_email = Client::find($client->client_id)->email;
             $token = Uuid::generate()->string;
-            Mail::to($client_email)->send(new SurveyEmail($template, $token));
+//            Mail::to($client_email)->send(new SurveyEmail($template, $token));
+//            SendSurveyEmailJob::dispatch($template, new SurveyEmail($template, $token))->delay(1);
+//            $this->dispatch(new SendSurveyEmailJob($template, $token));
+//            $this->dispatch(new SendSurveyEmailJob());
+            $this->dispatch(new SendSurveyEmailJob($template,$token,$client_email));
             Allocation::where('client_id', $client->client_id)->update(['email_sent' => 1]);
+
         }
 
     }
@@ -88,7 +111,11 @@ class SendSurveyEmailController extends Controller
             $template = Template::find($other->template_id);
             $email = unserialize($other->others);
             $token = Uuid::generate()->string;
-            Mail::to($email)->send(new SurveyEmail($template, $token));
+//            Mail::to($email)->send(new SurveyEmail($template, $token));
+//            $this->dispatch(new SendSurveyEmailJob($template, $token));
+//            SendSurveyEmailJob::dispatch(new SurveyEmail($template, $token))->delay(1);
+//            $this->dispatch(new SendSurveyEmailJob());
+            $this->dispatch(new SendSurveyEmailJob($template,$token,$email));
             Allocation::whereNull(['client_id', 'user_id'])->update(['email_sent' => 1]);
         }
 
--- app/Jobs/SendSurveyEmailJob.php
+++ app/Jobs/SendSurveyEmailJob.php
@@ -7,9 +7,12 @@ use App\Models\Template;
 use Illuminate\Bus\Queueable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Mail\Mailable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use App\Mail\SurveyEmail;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 
 class SendSurveyEmailJob implements ShouldQueue
 {
@@ -20,25 +23,26 @@ class SendSurveyEmailJob implements ShouldQueue
      *
      * @return void
      */
-    public $template;
-    public $token;
-    public $id;
-    public function __construct(Template $template, $token, $id)
+    protected $template;
+    protected $token;
+    protected $email;
+    public function __construct(Template $template, $token, $email)
     {
         $this->template = $template;
         $this->token = $token;
-        $this->id = $id;
+        $this->email = $email;
     }
 
     /**
      * Execute the job.
      *
-     * @param $id
      * @return void
      */
-    public function handle($id)
+    public function handle()
     {
-      return app(SendSurveyEmailController::class)->emailSurvey($id);
+        Log::info("job dispached");
+//      return app(SendSurveyEmailController::class)->emailSurvey($this->template->id);
+        Mail::to($this->email)->send(new SurveyEmail($this->template, $this->token));
 
 
     }
