575e6b200194c5c36ac2e9a6bdf1a3af244a59ce
carolkoi <gitongacarol11@gmail>
2019-11-24 11:19:48 +0300

    delaying mails

--- app/Http/Controllers/Allocation/SendSurveyEmailController.php
+++ app/Http/Controllers/Allocation/SendSurveyEmailController.php
@@ -71,7 +71,7 @@ class SendSurveyEmailController extends Controller
             $template = Template::find($staff->template_id);
             $staff_email = User::find($staff->user_id)->email;
             $token = Uuid::generate()->string;
-            $this->dispatch(new SendSurveyEmailJob($template,$token,$staff_email));
+            $this->dispatch(new SendSurveyEmailJob($template,$token,$staff_email))->sleep(1);
             Allocation::where('user_id', $staff->user_id)->update(['email_sent' => 1]);
 
         }
@@ -88,7 +88,7 @@ class SendSurveyEmailController extends Controller
             $template = Template::find($client->template_id);
             $client_email = Client::find($client->client_id)->email;
             $token = Uuid::generate()->string;
-            $this->dispatch(new SendSurveyEmailJob($template,$token,$client_email));
+            $this->dispatch(new SendSurveyEmailJob($template,$token,$client_email))->sleep(1);
             Allocation::where('client_id', $client->client_id)->update(['email_sent' => 1]);
 
         }
@@ -105,7 +105,7 @@ class SendSurveyEmailController extends Controller
             $template = Template::find($other->template_id);
             $email = unserialize($other->others);
             $token = Uuid::generate()->string;
-            $this->dispatch(new SendSurveyEmailJob($template,$token,$email));
+            $this->dispatch(new SendSurveyEmailJob($template,$token,$email))->sleep(1);
             Allocation::whereNull(['client_id', 'user_id'])->update(['email_sent' => 1]);
         }
 
