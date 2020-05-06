24f2b94f8b7a42a6f699742b9b2b01fcac68bb26
Caroline Gitonga <caroline.gitonga@switchlinkafrica.co.ke>
2020-05-06 13:48:04 +0300

    yyy

--- resources/views/transactions/datatables_actions.blade.php
+++ resources/views/transactions/datatables_actions.blade.php
@@ -1,7 +1,7 @@
 {{--{!! Form::open(['route' => ['transactions.destroy', $iso_id], 'method' => 'delete']) !!}--}}
 @if($res_field48 === "COMPLETED" OR $res_field48 === "FAILED" OR $res_field48 === "AML-APPROVED" OR Auth::check() && auth()->user()->cannot('Update transactions'))
-    <a href="{{ route('transactions.show', $iso_id) }}" class='btn btn-primary'>
-        Show
+    <a href="{{ route('transactions.show', $iso_id) }}" class='btn btn-primary btn-sm'>
+        <i class="glyphicon glyphicon-eye-open"></i>
     </a>
     @elseif(Auth::check() && auth()->user()->can('Update transactions'))
 <div class='btn-group'>
