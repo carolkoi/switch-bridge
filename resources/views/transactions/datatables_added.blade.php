eed2eab00e1f2ef0e35199d1b4e5fb122036d7b3
Caroline Gitonga <caroline.gitonga@switchlinkafrica.co.ke>
2020-05-14 10:32:42 +0300

    fixing curent time

--- resources/views/transactions/datatables_added.blade.php
+++ resources/views/transactions/datatables_added.blade.php
@@ -1,2 +1,4 @@
-{{ date('Y-m-d H:i:s', $date_time_added / 1000)
-}}
+{{--{{ date('Y-m-d H:i:s', $date_time_added / 1000)--}}
+{{--}}--}}
+{{date('Y-m-d H:i:s',strtotime('+3 hours',strtotime(date('Y-m-d H:i:s', $date_time_added / 1000))))}}
+{{--{{strtotime(date('Y-m-d H:i:s', $date_time_added / 1000))}}--}}
