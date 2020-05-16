ee4978d539c86b4aa50d2656decd2e492df851aa
Caroline Gitonga <caroline.gitonga@switchlinkafrica.co.ke>
2020-05-16 09:56:20 +0300

    sdmhfdhgh

--- resources/views/transactions/table.blade.php
+++ resources/views/transactions/table.blade.php
@@ -2,7 +2,7 @@
     @include('layouts.datatables_css')
 @endsection
 
-{!! $dataTable->table(['width' => '100%', 'class' => 'table table-striped table-bordered', 'data-page-length' => '50']) !!}
+{!! $dataTable->table(['width' => '100%', 'class' => 'table table-striped table-bordered', 'data-page-length' => '50', 'data-page-size' => '50']) !!}
 
 @section('scripts')
     @include('layouts.datatables_js')
@@ -10,17 +10,18 @@
     <script>
         jQuery(document).ready(function () {
         let table = $('#dataTableBuilder').DataTable({
-            "processing": true, //process it
-            "serverSide": true, //make it server side
+            // "processing": true, //process it
+            // "serverSide": true, //make it server side
             pageLength: 50,
             retrieve: true,
             ajax: "data.json"
         });
 
-            setInterval( function () {
-            table.ajax.reload(); // user paging is not reset on reload
-        }, 10000 );
+        //     setInterval( function () {
+        //     table.ajax.reload(); // user paging is not reset on reload
+        // }, 10000 );
         });
     </script>
 
 @endsection
+
