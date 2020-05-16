8c0b701c27597a4fc876bc6b86b89201c8eb25af
Caroline Gitonga <caroline.gitonga@switchlinkafrica.co.ke>
2020-05-16 09:15:57 +0300

    pagination

--- resources/views/transactions/table.blade.php
+++ resources/views/transactions/table.blade.php
@@ -2,7 +2,7 @@
     @include('layouts.datatables_css')
 @endsection
 
-{!! $dataTable->table(['width' => '100%', 'class' => 'table table-striped table-bordered']) !!}
+{!! $dataTable->table(['width' => '100%', 'class' => 'table table-striped table-bordered', 'data-page-length'=>'50']) !!}
 
 @section('scripts')
     @include('layouts.datatables_js')
@@ -12,9 +12,12 @@
         let table = $('#dataTableBuilder').DataTable( {
             retrieve: true,
             ajax: "data.json",
-            paging: false,
+            paging: true,
             info: false,
             scrollY: 400,
+            "pageLength": 100,
+            "iDisplayLength": 100,
+            "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]]
 
         } );
 
