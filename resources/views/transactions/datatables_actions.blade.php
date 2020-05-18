954a982f100a411055521475337bdbdc6fa06ac4
Caroline Gitonga <caroline.gitonga@switchlinkafrica.co.ke>
2020-05-18 11:20:41 +0300

    handling approval of txns

--- resources/views/transactions/datatables_actions.blade.php
+++ resources/views/transactions/datatables_actions.blade.php
@@ -1,11 +1,11 @@
-{{--{!! Form::open(['route' => ['transactions.destroy', $iso_id], 'method' => 'delete']) !!}--}}
-{{--@if($res_field48 === "COMPLETED" OR $res_field48 === "FAILED"--}}
-{{--OR (\App\Models\SessionTxn::where('txn_id', $iso_id)->exists() == true && (\WizPack\Workflow\Models\Approvals::where('model_id', $iso_id)->first()['approved']) == false)--}}
-{{--OR Auth::check() && auth()->user()->cannot('Can Update Transaction'))--}}
-{{--    <a href="{{ route('transactions.show', $iso_id) }}" class='btn btn-primary btn-sm'>--}}
-{{--        <i class="glyphicon glyphicon-eye-open"></i>--}}
-{{--    </a>--}}
-{{--@elseif(Auth::check() && auth()->user()->can('Can Update Transaction') OR ($req_field41 === "CASH" && $res_field48 === "AML-APPROVED") OR  $res_field48 === "AML-LISTED")--}}
+{!! Form::open(['route' => ['transactions.destroy', $iso_id], 'method' => 'delete']) !!}
+@if($res_field48 === "COMPLETED" OR $res_field48 === "FAILED"
+OR (\App\Models\SessionTxn::where('txn_id', $iso_id)->exists() == true && (\WizPack\Workflow\Models\Approvals::where('model_id', $iso_id)->first()['approved']) == true)
+OR Auth::check() && auth()->user()->cannot('Can Update Transaction'))
+    <a href="{{ route('transactions.show', $iso_id) }}" class='btn btn-primary btn-sm'>
+        <i class="glyphicon glyphicon-eye-open"></i>
+    </a>
+@elseif(Auth::check() && auth()->user()->can('Can Update Transaction') OR ($req_field41 === "CASH" && $res_field48 === "AML-APPROVED") OR  $res_field48 === "AML-LISTED")
 
     <div class='btn-group'>
     <a href="{{ route('transactions.show', $iso_id) }}" class='btn btn-primary btn-sm'>
@@ -16,7 +16,7 @@
         <i class="glyphicon glyphicon-edit"></i>
     </a>
 </div>
-{{--@endif--}}
+@endif
 
 {{--{!! Form::close() !!}--}}
 {{--@else--}}
