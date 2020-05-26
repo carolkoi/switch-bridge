cfc4bd5d52f7cefceb0f866d87fa9d4202786248
Caroline Gitonga <caroline.gitonga@switchlinkafrica.co.ke>
2020-05-26 15:27:14 +0300

    implementing filter date range on all grids: moment()

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -74,8 +74,8 @@
                     firstDay: 1,
                 },
                 ranges: {
-                    'Today': [moment(), moment()],
-                    'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                    'Today': [moment(), moment().add(1, 'days')],
+                    'Yesterday': [moment().subtract(1, 'days'), moment()],
                     'Last 7 Days': [moment().subtract(6, 'days'), moment()],
                     'Last 30 Days': [moment().subtract(29, 'days'), moment()],
                     'This Month': [moment().startOf('month'), moment().endOf('month')],
