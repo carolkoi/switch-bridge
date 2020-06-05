67d329071ca07138c632847b1c18a436887de871
Caroline Gitonga <caroline.gitonga@switchlinkafrica.co.ke>
2020-06-05 10:37:17 +0300

    fixing the filter

--- app/DataTables/Scopes/TransactionDataTableScope.php
+++ app/DataTables/Scopes/TransactionDataTableScope.php
@@ -17,8 +17,13 @@ class TransactionDataTableScope implements DataTableScope
         if (request()->has('from-to') || request()->input('filter-partner')) {
             $partner = request()->input('filter-partner');
             $date = explode(" - ", request()->input('from-to', ""));
-            $date1 = strtotime(trim($date[0])) * 1000;
-            $date2 = strtotime(trim($date[1])) * 1000;
+            $date1 =strtotime('+3 hours',strtotime(date('Y-m-d H:i:s', strtotime(trim($date[0])))))*1000;
+            $date2 =strtotime('+3 hours',strtotime(date('Y-m-d H:i:s', strtotime(trim($date[1])))))*1000;
+
+//            $date11 = strtotime(trim($date[0])) * 1000;
+//            $date21 = strtotime(trim($date[1])) * 1000;
+//            dd($date1, $date2, $date11, $date21);
+
             return $query->whereBetween('date_time_added', array($date1, $date2))->where('req_field123', $partner);
         }else
             return $query;
