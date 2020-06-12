81e14b43ffb348da4f9294e409f3af33704ecd29
Caroline Gitonga <caroline.gitonga@switchlinkafrica.co.ke>
2020-06-12 10:26:38 +0300

    ordering successful txn in dat modified

--- app/DataTables/SuccessTransactionsDataTable.php
+++ app/DataTables/SuccessTransactionsDataTable.php
@@ -81,7 +81,7 @@ class SuccessTransactionsDataTable extends DataTable
      */
     public function query(Transactions $model)
     {
-        return $model->orderBy('date_time_added', 'desc')->where('res_field48', 'COMPLETED')->newQuery();
+        return $model->orderBy('date_time_modified', 'desc')->where('res_field48', 'COMPLETED')->newQuery();
     }
 
     /**
