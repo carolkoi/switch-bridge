7f19c5da21d188be92a8cc20c79c2fffb6636447
carolkoi <gitongacarol11@gmail.com>
2020-12-06 02:41:05 +0300

    resolving https

--- resources/views/layouts/datatables_js.blade.php
+++ resources/views/layouts/datatables_js.blade.php
@@ -7,7 +7,7 @@
     <script src="{{ URL::asset('DataTables/js/buttons.colVis.min.js') }}"></script>
     <script src="{{ URL::asset('vendor/datatables/buttons.server-side.js') }}"></script>
     @else
-    <script src="{{\App\Helpers::assetToggle()}}DataTables/js/jquery.dataTables.min.js"></script>
+    <script src="{{ URL::asset('DataTables/js/jquery.dataTables.min.js') }}"></script>
     <script src="{{\App\Helpers::assetToggle()}}DataTables/js/dataTables.bootstrap.min.js"></script>
     <script src="{{\App\Helpers::assetToggle()}}DataTables/js/dataTables.buttons.min.js"></script>
     <script src="{{\App\Helpers::assetToggle()}}DataTables/js/buttons.bootstrap.min.js"></script>
@@ -15,3 +15,9 @@
     <script src="{{\App\Helpers::assetToggle()}}vendor/datatables/buttons.server-side.js"></script>
     @endif
 
+{{--<script src="{{\App\Helpers::assetToggle()}}DataTables/js/jquery.dataTables.min.js"></script>--}}
+{{--<script src="{{\App\Helpers::assetToggle()}}DataTables/js/dataTables.bootstrap.min.js"></script>--}}
+{{--<script src="{{\App\Helpers::assetToggle()}}DataTables/js/dataTables.buttons.min.js"></script>--}}
+{{--<script src="{{\App\Helpers::assetToggle()}}DataTables/js/buttons.bootstrap.min.js"></script>--}}
+{{--<script src="{{\App\Helpers::assetToggle()}}DataTables/js/buttons.colVis.min.js"></script>--}}
+{{--<script src="{{\App\Helpers::assetToggle()}}vendor/datatables/buttons.server-side.js"></script>--}}
