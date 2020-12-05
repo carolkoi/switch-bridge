0687a3e31be66efc4c29ab88b27f24221dce69a9
carolkoi <gitongacarol11@gmail.com>
2020-12-05 23:34:39 +0300

    datatables assets

--- resources/views/layouts/datatables_css.blade.php
+++ resources/views/layouts/datatables_css.blade.php
@@ -1,3 +1,9 @@
 <!-- DataTable Bootstrap -->
-<link href="{{URL::asset('DataTables/css/buttons.bootstrap.min.css')}}" rel="stylesheet">
-<link rel="stylesheet" href="{{URL::asset('DataTables/css/dataTables.bootstrap.min.css')}}">
+@if(\App\Helpers::getEnv() == "local")
+    <link href="{{URL::asset('DataTables/css/buttons.bootstrap.min.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="{{URL::asset('DataTables/css/dataTables.bootstrap.min.css')}}">
+    @else
+    <link href="{{\App\Helpers::assetToggle()}}DataTables/css/buttons.bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{\App\Helpers::assetToggle()}}DataTables/css/dataTables.bootstrap.min.css">
+    @endif
+
--- resources/views/layouts/datatables_js.blade.php
+++ resources/views/layouts/datatables_js.blade.php
@@ -1,7 +1,17 @@
 <!-- Datatables -->
-<script src="{{ URL::asset('DataTables/js/jquery.dataTables.min.js') }}"></script>
-<script src="{{ URL::asset('DataTables/js/dataTables.bootstrap.min.js') }}"></script>
-<script src="{{ URL::asset('DataTables/js/dataTables.buttons.min.js') }}"></script>
-<script src="{{ URL::asset('DataTables/js/buttons.bootstrap.min.js') }}"></script>
-<script src="{{ URL::asset('DataTables/js/buttons.colVis.min.js') }}"></script>
-<script src="{{ URL::asset('vendor/datatables/buttons.server-side.js') }}"></script>
+@if(\App\Helpers::getEnv() == "local")
+    <script src="{{ URL::asset('DataTables/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ URL::asset('DataTables/js/dataTables.bootstrap.min.js') }}"></script>
+    <script src="{{ URL::asset('DataTables/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ URL::asset('DataTables/js/buttons.bootstrap.min.js') }}"></script>
+    <script src="{{ URL::asset('DataTables/js/buttons.colVis.min.js') }}"></script>
+    <script src="{{ URL::asset('vendor/datatables/buttons.server-side.js') }}"></script>
+    @else
+    <script src="{{\App\Helpers::assetToggle()}}DataTables/js/jquery.dataTables.min.js"></script>
+    <script src="{{\App\Helpers::assetToggle()}}DataTables/js/dataTables.bootstrap.min.js"></script>
+    <script src="{{\App\Helpers::assetToggle()}}DataTables/js/dataTables.buttons.min.js"></script>
+    <script src="{{\App\Helpers::assetToggle()}}DataTables/js/buttons.bootstrap.min.js"></script>
+    <script src="{{\App\Helpers::assetToggle()}}DataTables/js/buttons.colVis.min.js"></script>
+    <script src="vendor/datatables/buttons.server-side.js"></script>
+    @endif
+
