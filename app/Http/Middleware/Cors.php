a48317f8d4f91c6fc281237e8246d4f39f5a8607
carolkoi <gitongacarol11@gmail.com>
2020-12-06 02:05:10 +0300

    resolving https

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,8 +15,8 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        //header('Access-Control-Allow-Origin:  *');
-        header('Access-Control-Allow-Origin:  https://dev.slafrica.net:6810');
+        header('Access-Control-Allow-Origin:  *');
+//        header('Access-Control-Allow-Origin:  https://dev.slafrica.net:6810');
         header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
         header('Access-Control-Allow-Methods:  POST, PUT');
         return $next($request);
