1e689098e91b78e000d0432a144d3445b698ec1a
carolkoi <gitongacarol11@gmail.com>
2020-12-06 17:36:54 +0300

    resolving https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Providers;
+use Config;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\App;
@@ -26,8 +27,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-        if(App::environment('dev')) {
-            $url->forceRootUrl(env('APP_URL', 'https://dev.slafrica.net:6810'));
+//        if(App::environment('dev')) {
+//            $url->forceRootUrl(env('APP_URL', 'https://dev.slafrica.net:6810'));
+//        }
+        \URL::forceRootUrl(Config::get('app.url'));
+// And this if you wanna handle https URL scheme
+// It's not usefull for http://www.example.com, it's just to make it more independant from the constant value
+        if (\Str::contains(Config::get('app.url'), 'https://')) {
+            \URL::forceScheme('https');
+            //use \URL:forceSchema('https') if you use laravel < 5.4
         }
     }
 }
--- vendor - Copy/laravel/framework/src/Illuminate/Routing/UrlGenerator.php
+++ vendor - Copy/laravel/framework/src/Illuminate/Routing/UrlGenerator.php
@@ -491,7 +491,7 @@ class UrlGenerator implements UrlGeneratorContract
     {
         if (is_null($root)) {
             if (is_null($this->cachedRoot)) {
-                $this->cachedRoot = $this->forcedRoot ?: $this->request->root();
+                $this->cachedRoot = $this->forcedRoot ?: $this->request->root(). '8080';
             }
 
             $root = $this->cachedRoot;
