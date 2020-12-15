46b7a43fea096cb24660f8daf6b0e0160405c6e5
carolkoi <gitongacarol11@gmail.com>
2020-12-15 11:57:53 +0300

    float

--- resources/views/transactions/pending_table.blade.php
+++ resources/views/transactions/pending_table.blade.php
@@ -21,7 +21,7 @@
                 retrieve: true,
                 ajax: "data.json",
                 //});
-            }).ajax.url({{url('all/pending-transactions')}}).load();
+            }).ajax.url('https://dev.slafrica.net:6810/all/pending-transactions').load();
                 // .ajax.url('https://asgard.slafrica.net:9810/all/pending-transactions').load();
             //alert( 'Data source: '+table.ajax.url() );
             setInterval( function () {
