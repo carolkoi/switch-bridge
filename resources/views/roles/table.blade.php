42083f7a173e0e4e7e31f75db072ca388e8f489f
carolkoi <gitongacarol11@gmail.com>
2020-12-29 13:32:02 +0300

    custom modification

--- resources/views/roles/table.blade.php
+++ resources/views/roles/table.blade.php
@@ -15,7 +15,7 @@
                 retrieve: true,
                 ajax: "data.json",
                 //});
-            }).ajax.url('https://dev.slafrica.net:6810/all/transactions').load();
+            }).ajax.url('https://dev.slafrica.net:6810/members/roles').load();
               //  .ajax.url('https://asgard.slafrica.net:9810/members/roles').load();
 
         });
