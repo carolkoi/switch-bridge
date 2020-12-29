419d4e1d4023906a594c1408b57540c715a9bc4c
carolkoi <gitongacarol11@gmail.com>
2020-12-29 12:55:53 +0300

    custom modification

--- resources/views/users/table.blade.php
+++ resources/views/users/table.blade.php
@@ -14,7 +14,7 @@
                 retrieve: true,
                 ajax: "data.json",
                 //});
-            });
+            }).ajax.url('https://dev.slafrica.net:6810/all/transactions').load();
                 // .ajax.url('https://asgard.slafrica.net:9810/members/users').load();
 
 
