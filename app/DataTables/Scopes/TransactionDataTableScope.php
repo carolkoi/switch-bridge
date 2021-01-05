614bc993bb214d8e02de54d6ea296d83ce4283a0
carolkoi <gitongacarol11@gmail.com>
2021-01-05 11:53:09 +0300

    UI makeover

--- app/DataTables/Scopes/TransactionDataTableScope.php
+++ app/DataTables/Scopes/TransactionDataTableScope.php
@@ -16,20 +16,20 @@ class TransactionDataTableScope implements DataTableScope
      */
     public function apply($query)
     {
-        $ddate = "2017-1-31";
-        $date = new DateTime();
-        $week = $date->format("W");
-        $year = $date->format("o");
-        function etStartAndEndDate($week, $year) {
-            $dto = new DateTime();
-            $dto->setISODate($year, $week);
-            $ret['week_start'] = $dto->format('Y-m-d');
-            $dto->modify('+6 days');
-            $ret['week_end'] = Carbon::now()->format('Y-m-d');
-            return $ret;
-        }
-        $day=etStartAndEndDate($week-1,$year);
-//        dd($day);
+//        $ddate = "2017-1-31";
+//        $date = new DateTime();
+//        $week = $date->format("W");
+//        $year = $date->format("o");
+//        function etStartAndEndDate($week, $year) {
+//            $dto = new DateTime();
+//            $dto->setISODate($year, $week);
+//            $ret['week_start'] = $dto->format('Y-m-d');
+//            $dto->modify('+6 days');
+//            $ret['week_end'] = Carbon::now()->format('Y-m-d');
+//            return $ret;
+//        }
+//        $day=etStartAndEndDate($week-1,$year);
+////        dd($day);
 
         if (request()->has('filter-partner') && request()->has('txn-type')
             && request()->has('report_time') && request()->has('from-to')){
@@ -106,6 +106,6 @@ class TransactionDataTableScope implements DataTableScope
             $txnType = request()->input('txn-type');
             return $query->where('req_field41', 'LIKE', "%$txnType%");
         }
-            return $query->whereBetween('paid_out_date', array($day['week_start'], $day['week_end']));
+            return $query->take(1000);
     }
 }
