5279656c3e1154952c32ec31bb0839cc618c756e
carolkoi <gitongacarol11@gmail.com>
2021-01-07 02:31:19 +0300

    ui

--- app/Models/Transactions.php
+++ app/Models/Transactions.php
@@ -2219,7 +2219,7 @@ class Transactions extends Model implements ApprovableInterface
             $txnType = request()->input('txn-type');
             return $query->where('req_field41', 'LIKE', "%$txnType%");
         }
-        return $query->whereBetween('paid_out_date', array($day['start'], $day['end']));
-//        return $query;
+//        return $query->whereBetween('paid_out_date', array($day['start'], $day['end']));
+        return $query;
     }
 }
