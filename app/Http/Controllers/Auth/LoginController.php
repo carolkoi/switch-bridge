0bbd7fa83552779cf35cb2143a26331ec42b1afe
carolkoi <gitongacarol11@gmail.com>
2021-01-07 08:35:33 +0300

    ui

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/all/transactions';
 
     /**
      * Create a new controller instance.
