bde589ccf812ea58e2e0c8d7a4545e6a7499df1e
carolkoi <gitongacarol11@gmail.com>
2021-01-10 15:10:31 +0300

    speed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,19 +30,14 @@ class HomeController extends Controller
         $now =  Carbon::now()->format('Y-m-d');
         $to = Carbon::now()->addDays(1)->format('Y-m-d');
         $date = array('start' => $from, 'today' => $now, 'end' => $to);
-//        dd($date);
-        $all_transactions = Transactions::transactionsByCompany()->take(2000)->get();
-        //$no_of_trns = Transactions::transactionsByCompany()->get()->count(); =
 
-        $today_transactions = Transactions::transactionsByCompany()->whereDate('created_at', Carbon::today())->get();
+//        $today_transactions = Transactions::transactionsByCompany()->whereDate('created_at', Carbon::today())->get();
         $today_transactions = Transactions::transactionsByCompany()
             ->whereBetween('created_at', array($date['today'], $date['end']))->get();
 
-        $yesterday = date("Y-m-d m:h:s", strtotime( '-1 days' ) );
         $yesterday_txns = Transactions::transactionsByCompany()
             ->whereBetween('created_at', array($date['start'], $date['today']))->get();
 
-        $transactions = Transactions::transactionsByCompany()->orderBy('iso_id', 'desc')->take(2000)->paginate(30);
 
         $page = $request->has('page') ? $request->get('page') : 1;
         $limit = $request->has('limit') ? $request->get('limit') : 10;
