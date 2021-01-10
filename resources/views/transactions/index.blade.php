0e88646f9c432e4c74ee8838f88bdc74a4b6272a
carolkoi <gitongacarol11@gmail.com>
2021-01-10 14:19:32 +0300

    speed

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -37,9 +37,9 @@
                                 {{--                                        <label>SELECT PARTNER</label>--}}
                                 <div class="form-group" id="filter-partner-id" style="">
                                     @if(Auth::check() && Auth::user()->company_id == 9)
-                                        <select name="filter-partner" class="form-control param"
+                                        <select name="filter-partner" class="form-control param select2"
                                                 id="filter-partner">
-                                            <option>SELECT PARTNER</option>
+                                            <option disabled>SELECT PARTNER</option>
                                             @foreach($upesi_partners as $partner)
                                                 <option
                                                     value="{{$partner->partner_name}}">{{$partner->partner_name}}
@@ -68,7 +68,7 @@
                                         <select name="req_field123" class="form-control param select2"
                                                 id="filter-partner">
                                             {{--                                                <option>SELECT PARTNER</option>--}}
-                                            <option value="" selected disabled>SELECT PARTNER</option>
+                                            <option disabled>SELECT PARTNER</option>
                                             @foreach($partners as $partner)
                                                 <option
                                                     value="{{$partner->partner_name}}">{{$partner->partner_name}}</option>
@@ -79,18 +79,14 @@
                                 </div>
                             </div>
                             <div class="col-md-3">
-                                {{--                                        <label>SELECT TRANSACTION TYPE</label>--}}
 
                                 <div class="form-group" id="txn_type_id" style="">
                                     <select name="req_field41" id="txn_type" class="form-control param select2">
-                                        <option value="" selected disabled>SELECT TYPE</option>
+                                        <option selected disabled>SELECT TYPE</option>
                                         @foreach($txnTypes as $key => $txnType)
                                             <option value="{{ trim($txnType) }}">{{ trim($txnType) }}</option>
                                         @endforeach
                                     </select>
-                                    {{--                                        {{Form::label('', 'Select Partner')}}--}}
-                                    {{--                                        {{Form::select('txn-type', $txnTypes, null, ['class' => 'form-control param select2',--}}
-                                    {{--                                    'id' => 'txn_type'])}}--}}
 
                                 </div>
                             </div>
@@ -108,42 +104,15 @@
                                         <button type="button" id="dateSearch" class="btn btn-sm btn-primary">Search</button>
                                     </div>
 
-                                    {{--                                    <div class="input-group date">--}}
-{{--                                        <div class="input-group-addon">--}}
-{{--                                            <i class="fa fa-calendar"></i>--}}
-{{--                                        </div>--}}
 
-{{--                                        <input type="text" name="fromto" class="form-control param" id="date_filter"--}}
-{{--                                               autocomplete="off">--}}
-{{--                                        --}}{{--                                            {!! Form::text('paid_out_date', null, ['class' => 'form-control', 'autocomplete' => 'off', 'id' => 'paid-out-id']) !!}--}}
-{{--                                    </div>--}}
-                                    {{--                                            <label>SELECT DATE</label>--}}
-
-{{--                                    <select name="report_time" id="report_time_id" class="form-control param select2">--}}
-{{--                                        <option>SELECT REPORTING DATE</option>--}}
-{{--                                        <option value="trn_date">TRANSACTION DATE</option>--}}
-{{--                                        <option value="paid_date">PAID OUT DATE</option>--}}
-
-{{--                                    </select>--}}
                                 </div>
                                 <br><br>
-                                {{--                                        <input type="text" name="from-to" class="form-control param" id="date_filter" autocomplete="off">--}}
-                                {{--                                        {!! Form::label('paid_out_date', 'Paid Out Date:') !!}--}}
 
-{{--                                <span class="">--}}
-{{--                                        <div class="btn-group" id="button_filter" style="display: none">--}}
-{{--                    <button type="button" name="filter" id="filter-id" class=" btn btn-primary">Filter</button>--}}
-{{--                    <button type="button" name="refresh" id="refresh" class="btn btn-default">Refresh</button>--}}
-{{--                </div>--}}
-{{--                                    <input type="submit" class="btn btn-primary" value="Filter" id="filter-id">--}}
-{{--                                </span>--}}
                             </form>
 
                         </div>
-                        {{--                            </div>--}}
                     </div>
 
-                    {{--                    </div>--}}
 
                 </div>
                 <br>
