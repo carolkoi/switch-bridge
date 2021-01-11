7ba798d110dd1b0f61d40daa5793b19a065974af
carolkoi <gitongacarol11@gmail.com>
2021-01-11 16:25:36 +0300

    normal tables

--- resources/views/roles/table.blade.php
+++ resources/views/roles/table.blade.php
@@ -15,8 +15,7 @@
                 retrieve: true,
                 ajax: "data.json",
                 //});
-            });
-              //  .ajax.url('https://asgard.slafrica.net:9810/members/roles').load();
+            }).ajax.url('https://asgard.slafrica.net:9810/members/roles').load();
 
         });
     </script>
--- resources/views/users/table.blade.php
+++ resources/views/users/table.blade.php
@@ -14,8 +14,7 @@
                 retrieve: true,
                 ajax: "data.json",
                 //});
-            });
-                // .ajax.url('https://asgard.slafrica.net:9810/members/users').load();
+            }).ajax.url('https://asgard.slafrica.net:9810/members/users').load();
         });
     </script>
 
